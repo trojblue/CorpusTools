52d540c3a6a04ef4d9666b650009826ae0272328
trojblue <trojblue@gmail.com>
2021-01-23 04:58:17 +0800

    done: parsing data into objects

--- classes.py
+++ classes.py
@@ -29,38 +29,27 @@ class msgParser():
 
         self.sender = lines[5]
 
-        tryer = []
+        self.add_messages(lines)
 
-        for i in range(8, len(lines)-2):
-            curr_line = lines[i][:-1] # 去掉回车
+    def add_messages(self, lines):
+        """读取lines, 添加single messages object到self
+        """
+        meta_line = None
+        for i in range(8, len(lines) - 2):
+
+            curr_line = lines[i][:-1]  # 去掉回车
+
             if is_meta(curr_line):
-                date_str = curr_line[:19]
-                name_str = curr_line[20:]
-            elif curr_line is None:
-                continue
-            else:
-                # text
-                msg_str = curr
+                # 处理前面的meta_line
 
-            meta = lines[i][:-1]
-            msg = lines[i+1][:-1]
+                if meta_line is not None:
+                    prev_meta = lines[meta_line][:-1]
+                    prev_msgs = lines[meta_line + 1: i]
+                    prev_candidates = " ".join(prev_msgs).replace("\n", ' ').strip(' ')
+                    self.msg_list.append(singleMsg(prev_meta, prev_candidates))
 
-            self.msg_list.append(singleMsg(meta, msg))
+                meta_line = i
 
-
-
-            # if i%2 == 0: # 偶数: metadata
-            #     pass
-            #
-            # else:   # 奇数: 信息
-            #     pass
-            #
-            #
-            # if lines[i].strip("\n") == "[图片]":
-            #     lines[i - 2], lines[i - 1], lines[i] = "", "", ""
-
-
-        print("D")
 
 
 class singleMsg():
@@ -68,29 +57,40 @@ class singleMsg():
     > 2020-07-01 12:01:37 wjr 永不平等
     > 就是就是
     """
-    def __init__(self, meta:str, msg:str):
+
+    def __init__(self, meta: str, msg: str):
         self.date = None
         self.sender = None
         self.msg = msg
-        # self.parse_meta()
-        is_meta(meta)
+        self.parse_meta(meta)
 
-    def parse_meta(self):
-        """meta:
+    def __repr__(self):
+        return (show_time(self.date) + "    |    <" + self.sender + ">   " + self.msg)
+
+    def parse_meta(self, meta):
+        """meta: single line meta
         """
-        pass
+        date_time_obj = parse_time(meta[:19])
+        self.date = date_time_obj
+        self.sender = meta[20:]
 
 
-def is_meta(line:str):
+def is_meta(line: str):
     """判断是否是包含日期的meta栏:
         2018-07-10 10:32:51 null
         2018-06-15 4:04:50 王珺儒
         2020-06-21 8:39:17 王珺儒 只聊游戏和商品
     """
     re_string = r'^(?P<date>\d{4}-\d{2}-\d{2}\s\d{1,2}:\d{2}:\d{2})\s.*$'
-    for string in line:
-        if (re.match(re_string, string)):
-            print(string, "yes")
-        else:
-            print(string, "no")
+    if (re.match(re_string, line)):
+        return True
+    else:
+        return False
 
+
+def parse_time(date_string: str):
+    return datetime.strptime(date_string.strip(' '), '%Y-%m-%d %H:%M:%S')
+
+
+def show_time(date_obj):
+    return date_obj.strftime('%Y-%m-%d %H:%M:%S')
